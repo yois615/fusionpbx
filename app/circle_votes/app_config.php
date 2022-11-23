75f8b0dcc6fd833c5d1eec831ded359d15fe9870
yois615 <38441801+yois615@users.noreply.github.com>
2022-11-23 14:14:56 -0500

    [circle_vote]

---
+++ app/circle_votes/app_config.php
@@ -0,0 +1,40 @@
+<?php
+
+	//application details
+		$apps[$x]['name'] = 'Circle Votes';
+		$apps[$x]['uuid'] = '01e92702-6b4b-11ed-a1eb-0242ac1ef276';
+		$apps[$x]['category'] = '';
+		$apps[$x]['subcategory'] = '';
+		$apps[$x]['version'] = '';
+		$apps[$x]['license'] = 'Mozilla Public License 1.1';
+		$apps[$x]['url'] = 'http://www.fusionpbx.com';
+		$apps[$x]['description']['en-us'] = '';
+		$apps[$x]['description']['en-gb'] = '';
+
+	//permission details
+		$y = 0;
+		$apps[$x]['permissions'][$y]['name'] = 'circle_votes_view';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		//$apps[$x]['permissions'][$y]['groups'][] = 'admin';
+		$y++;
+		$apps[$x]['permissions'][$y]['name'] = 'circle_votes_delete';
+		$apps[$x]['permissions'][$y]['groups'][] = 'superadmin';
+		//$apps[$x]['permissions'][$y]['groups'][] = 'admin';
+		$y++;
+
+	//Votes
+		$y = 0;
+		$apps[$x]['db'][$y]['table']['name'] = 'circle_tt_votes';
+		$apps[$x]['db'][$y]['table']['parent'] = '';
+		$z = 0;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'vote';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'numeric';
+		$z++;
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'customer_id';
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'numeric';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'circle_customer';
+		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'customer_id';
+		$z++;
+
+?>
---
+++ app/circle_votes/circle_votes.php
@@ -0,0 +1,176 @@
+<?php
+/*
+	FusionPBX
+	Version: MPL 1.1
+
+	The contents of this file are subject to the Mozilla Public License Version
+	1.1 (the "License"); you may not use this file except in compliance with
+	the License. You may obtain a copy of the License at
+	http://www.mozilla.org/MPL/
+
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+
+	The Original Code is FusionPBX
+
+	The Initial Developer of the Original Code is
+	Mark J Crane <markjcrane@fusionpbx.com>
+	Portions created by the Initial Developer are Copyright (C) 2018 - 2019
+	the Initial Developer. All Rights Reserved.
+
+	Contributor(s):
+	Mark J Crane <markjcrane@fusionpbx.com>
+*/
+
+//includes
+	require_once "root.php";
+	require_once "resources/require.php";
+	require_once "resources/check_auth.php";
+	require_once "resources/paging.php";
+
+//check permissions
+	if (permission_exists('circle_votes_view')) {
+		//access granted
+	}
+	else {
+		echo "access denied";
+		exit;
+	}
+
+//add multi-lingual support
+	$language = new text;
+	$text = $language->get();
+
+//get the http post data
+	if (is_array($_POST['circle_votes'])) {
+		$action = $_POST['action'];
+		$search = $_POST['search'];
+		$circle_votes = $_POST['circle_votes'];
+	}
+
+//process the http post data by action
+	if ($action != '' && is_array($circle_votes) && @sizeof($circle_votes) != 0) {
+		switch ($action) {
+			case 'delete':
+				if (permission_exists('circle_votes_delete')) {
+					$obj = new bridges;
+					$obj->delete($bridges);
+				}
+				break;
+		}
+
+		header('Location: circle_votes.php'.($search != '' ? '?search='.urlencode($search) : null));
+		exit;
+	}
+
+//get order and order by
+	$order_by = $_GET["order_by"];
+	$order = $_GET["order"];
+
+
+/*get the count
+	$sql = "select count(bridge_uuid) from v_bridges ";
+	if ($_GET['show'] == "all" && permission_exists('bridge_all')) {
+		if (isset($sql_search)) {
+			$sql .= "where ".$sql_search;
+		}
+	}
+	else {
+		$sql .= "where (domain_uuid = :domain_uuid or domain_uuid is null) ";
+		if (isset($sql_search)) {
+			$sql .= "and ".$sql_search;
+		}
+		$parameters['domain_uuid'] = $domain_uuid;
+	}
+	$database = new database;
+	$num_rows = $database->select($sql, $parameters, 'column');
+*/
+//prepare to page the results
+	$rows_per_page = ($_SESSION['domain']['paging']['numeric'] != '') ? $_SESSION['domain']['paging']['numeric'] : 50;
+	$page = is_numeric($_GET['page']) ? $_GET['page'] : 0;
+	list($paging_controls, $rows_per_page) = paging($num_rows, $param, $rows_per_page);
+	list($paging_controls_mini, $rows_per_page) = paging($num_rows, $param, $rows_per_page, true);
+	$offset = $rows_per_page * $page;
+
+//get the list
+	$sql = "select vote,count(vote) FROM circle_tt_votes GROUP BY vote ORDER BY count DESC ";
+	$sql .= limit_offset($rows_per_page, $offset);
+	$database = new database;
+	$vote_results = $database->select($sql, $parameters, 'all');
+	unset($sql, $parameters);
+
+//create token
+	$object = new token;
+	$token = $object->create($_SERVER['PHP_SELF']);
+
+//include the header
+	$document['title'] = $text['title-circle-votes'];
+	require_once "resources/header.php";
+
+//show the content
+	echo "<div class='action_bar' id='action_bar'>\n";
+	echo "	<div class='heading'><b>".$text['title-circle-vote']." (".$num_rows.")</b></div>\n";
+	echo "	<div class='actions'>\n";
+	
+	if (permission_exists('bridge_delete')) {
+		echo button::create(['type'=>'button','label'=>$text['button-circle-vote-delete'],'icon'=>$_SESSION['theme']['button_icon_delete'],'name'=>'btn_delete','onclick'=>"modal_open('modal-delete','btn_delete');"]);
+	}
+	echo 		"<form id='form_search' class='inline' method='get'>\n";
+	if ($paging_controls_mini != '') {
+		echo 	"<span style='margin-left: 15px;'>".$paging_controls_mini."</span>\n";
+	}
+	echo "		</form>\n";
+	echo "	</div>\n";
+	echo "	<div style='clear: both;'></div>\n";
+	echo "</div>\n";
+
+	if (permission_exists('circle_votes_delete')) {
+		echo modal::create(['id'=>'modal-delete','type'=>'delete','actions'=>button::create(['type'=>'button','label'=>$text['button-continue'],'icon'=>'check','id'=>'btn_delete','style'=>'float: right; margin-left: 15px;','collapse'=>'never','onclick'=>"modal_close(); list_action_set('delete'); list_form_submit('form_list');"])]);
+	}
+
+	echo $text['title_description-bridge']."\n";
+	echo "<br /><br />\n";
+
+	echo "<form id='form_list' method='post'>\n";
+	echo "<input type='hidden' id='action' name='action' value=''>\n";
+	echo "<input type='hidden' name='search' value=\"".escape($search)."\">\n";
+
+	echo "<table class='list'>\n";
+	echo "<tr class='list-header'>\n";
+	
+	echo th_order_by('Count', $text['label-circle_votes_name'], $order_by, $order);
+	echo th_order_by('Number', $text['label-circle_votes_destination'], $order_by, $order);
+	echo "	<th class='hide-sm-dn'>".$text['label-circle_votes_description']."</th>\n";
+	echo "</tr>\n";
+
+	if (is_array($vote_results) && @sizeof($vote_results) != 0) {
+		$x = 0;
+		foreach ($vote_results as $row) {		
+			echo "<tr class='list-row' href='".$list_row_url."'>\n";
+			echo "	<td>\n";
+			echo "	</td>\n";
+			echo "	<td>".escape($row['bridge_destination'])."</td>\n";
+
+				echo "	<td class='center'>\n";
+				echo $text['label-'.$row['bridge_enabled']];
+			echo "	</td>\n";
+			echo "	<td class='description overflow hide-sm-dn'>".escape($row['bridge_description'])."</td>\n";
+			
+			echo "</tr>\n";
+			$x++;
+		}
+		unset($vote_results);
+	}
+
+	echo "</table>\n";
+	echo "<br />\n";
+	echo "<div align='center'>".$paging_controls."</div>\n";
+	echo "<input type='hidden' name='".$token['name']."' value='".$token['hash']."'>\n";
+	echo "</form>\n";
+
+//include the footer
+	require_once "resources/footer.php";
+
+?>
