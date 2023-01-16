2fc4cf3d851e3551f3d08ef0bcf26c3acc862097
yois615 <38441801+yois615@users.noreply.github.com>
2023-01-16 10:57:06 -0500

    UI fix

--- app/call_center_active/call_center_active_inc.php
+++ app/call_center_active/call_center_active_inc.php
@@ -383,7 +383,7 @@
 								$orig_command="{origination_caller_id_name=eavesdrop,origination_caller_id_number=".escape($q_caller_number)."}user/".escape($_SESSION['user']['extension'][0]['user'])."@".escape($_SESSION['domain_name'])." %26eavesdrop(".escape($session_uuid).")";
 								echo button::create(['type'=>'button','class'=>'link','label'=>$text['label-eavesdrop'],'onclick'=>"if (confirm('".$text['message-confirm']."')) { send_command('call_center_exec.php?command=eavesdrop&caller_id_number=".urlencode($caller_number)."&uuid=".urlencode($session_uuid)."'); } else { this.blur(); return false; }"]);
 							}
-							echo button::create(['type'=>'button','class'=>'link','label'=>$text['label-pickup'],'onclick'=>"if (confirm('".$text['message-confirm']."')) { send_command('call_center_exec.php?command=uuid_pickup&uuid=".urlencode($session_uuid)."'); } else { this.blur(); return false; }"]);
+							echo button::create(['type'=>'button','class'=>'link','label'=>$text['label-pickup'],'style'=>'margin-left: 15px;','onclick'=>"if (confirm('".$text['message-confirm']."')) { send_command('call_center_exec.php?command=uuid_pickup&uuid=".urlencode($session_uuid)."'); } else { this.blur(); return false; }"]);
 						}
 						else {
 							echo "&nbsp;";
@@ -443,7 +443,7 @@
 					echo "<td>".escape($retry_count)."&nbsp;</td>\n";
 					if (permission_exists('call_center_active_options') || permission_exists('call_center_active_pickup')) {
 						echo "<td>";
-						echo button::create(['type'=>'button','class'=>'link','label'=>$text['label-callback'],'style'=>'margin-left: 15px;','onclick'=>"if (confirm('".$text['message-confirm']."')) { send_command('call_center_exec.php?command=uuid_callback&uuid=".urlencode($call_uuid)."'); } else { this.blur(); return false; }"]);
+						echo button::create(['type'=>'button','class'=>'link','label'=>$text['label-callback'],'onclick'=>"if (confirm('".$text['message-confirm']."')) { send_command('call_center_exec.php?command=uuid_callback&uuid=".urlencode($call_uuid)."'); } else { this.blur(); return false; }"]);
 						echo "</td>";
 					}
 				}
